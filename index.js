106f4c15824156cca6f1f125ccca96428e2eaded
Souei <57272851+Soueii@users.noreply.github.com>
2024-02-25 14:46:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = 6;
+var result1 = Math.floor(Math.random() * randomNumber1 + 1);
+var result2 = Math.floor(Math.random() * randomNumber1 + 1);
+
+document
+  .querySelector(".dice .img1")
+  .setAttribute("src", "./images/dice" + result1 + ".png");
+
+document
+  .querySelector(".dice .img2")
+  .setAttribute("src", "./images/dice" + result2 + ".png");
+
+if (result1 === result2) {
+  document.querySelector("h1").innerHTML = "Draw";
+} else if (result1 > result2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
